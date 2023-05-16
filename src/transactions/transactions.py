03c569cdff526ce523e21acf1c4917d64fcce103
Matheus Rian <souzamatheusrian@gmail.com>
2023-05-16 12:39:04 -0300

    feat: change array to dictionary

--- src/transactions/transactions.py
+++ src/transactions/transactions.py
@@ -1,17 +1,25 @@
 class Transactions:
   def __init__(self) -> None:
-    self.items = []
+    self.items = {}
+    self.sizeItems = 0
 
-  def add(self, transaction):
-    self.items.append(transaction)
+  def add(self, category, transaction):
+    if (self.items.get(category)):
+      self.items[category].append(transaction)
+    else:
+      self.items[category] = [transaction]
 
   def removeByName(self, name):
-    for i, value in enumerate(self.get_items()):
-      if (value['name'] == name):
-        self.items.pop(i)
+    for key in self.items.keys():
+      for i, value in enumerate(self.items[key]):
+        if (value['name'] == name):
+          self.items[key].pop(i)
 
   def size(self):
-    return len(self.items)
+    for key in self.items.keys():
+      self.sizeItems += len(self.items[key])
+
+    return self.sizeItems
   
   def get_items(self):
     return self.items
