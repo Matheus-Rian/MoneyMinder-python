8cddbb3ef6b99ab4938e385a8c238fc1a1e81470
victor guilherme <guilherme@pop-os.localdomain>
2023-05-23 08:30:50 -0300

    refatorando itens por items

--- src/Porquinho/porquinho.py
+++ src/Porquinho/porquinho.py
@@ -1,26 +1,26 @@
 class Porquinho:
     def __init__(self):
-        self.itens = {}
-        self.tamanho_itens = 0
+        self.items = {}
+        self.tamanho_items = 0
     def criar_porquinho(self, nome):
-        if nome in self.itens:
+        if nome in self.items:
             print("Porquinho existente")
         else:
-            self.itens[nome] = 0
+            self.items[nome] = 0
     def update_valor(self, nome):
         if nome in self.items:
             v = float(input(f"Deseja adicionar qual valor para {nome}? Se quiser remover, digite com o "-" na frente do valor. "))
             self.items[nome] += v
             print(f'O valor atual do porquinho é: {self.items[nome]}')
     def tamanho(self):
-        self.tamanho_itens = len(self.itens)
-        return self.tamanho_itens
+        self.tamanho_items = len(self.items)
+        return self.tamanho_items
     def visualizar_nomes(self):
-        for nome in self.itens.keys():
+        for nome in self.items.keys():
             print(nome)
     def remover_nome(self, nome):
-        if nome in self.itens:
-            del self.itens[nome]
+        if nome in self.items:
+            del self.items[nome]
             print("Nome removido com sucesso.")
         else:
             print("Nome não encontrado.")
