70c10681d94f10e6ae38b7cd6f6a163a9cef038a
GabrielBza <Gabrielbzandrade@gmail.com>
2023-05-24 18:21:04 -0300

    feat: menu atualizado

--- src/menu.py
+++ src/menu.py
@@ -1,47 +1,46 @@
 def menu_principal():
-    def segundo_menu (primeira_escolha_de_funcao):
+    try:
+        #Função do menu após a primeira escolha do usuário
+        def segundo_menu (primeira_escolha_do_usuario):
+            print("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+            print("\t\t           Menu --> " + opcoes_do_primeiro_menu[primeira_escolha_do_usuario -1])
+            print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
+        #Função do menu após a segunda escolha do usuário
+        def terceiro_menu (primeira_escolha_do_usuario, segunda_escolha_do_usuario):
+            print("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+            print("\t        Menu --> " + opcoes_do_primeiro_menu[primeira_escolha_do_usuario -1] + opcoes_do_segundo_menu[segunda_escolha_do_usuario - 1])
+            print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
+
+        #Primeiro menu, onde o usuário ainda não escolheu nada
+        #A partir do número que o usuário inserir na variável "primeira_escolha_do_usuário", será correspondentemente acessado no vetor "opções_do_primeiro_menu" 
+        opcoes_do_primeiro_menu = ["Transações", "Filtrar Por Categoria", "Porquinho"]
         print("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-        print("\t\t           Menu --> " + opcoes_do_primeiro_menu[int(primeira_escolha_de_funcao) -1])
+        print("\t\t\t         Menu")
         print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
-    def terceiro_menu (primeira_escolha_de_funcao, segunda_escolha_de_funcao):
-        print("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-        print("\t        Menu --> " + opcoes_do_primeiro_menu[int(primeira_escolha_de_funcao) -1] + opcoes_do_segundo_menu[int(segunda_escolha_de_funcao) - 1])
-        print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
-    def quarto_menu (primeira_escolha_de_funcao, segunda_escolha_de_funcao, terceira_escolha_de_funcao):
-        print("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-        print("Menu --> " + opcoes_do_primeiro_menu[int(primeira_escolha_de_funcao) -1] + opcoes_do_segundo_menu[int(segunda_escolha_de_funcao) - 1] + opcoes_do_terceiro_menu[int(terceira_escolha_de_funcao) -1])
-        print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
+        print("Digite o número da ação que deseja realizar: \n[1]Transações \n[2]Filtrar Por Categoria \n[3]Porquinho \n")
+        primeira_escolha_do_usuario = int(input())
 
-    opcoes_do_primeiro_menu = ["Transações", "Filtrar Por Categoria", "Porquinho"]
-    print("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-    print("\t\t\t         Menu")
-    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
-    print("Digite o número da ação que deseja realizar: \n[1]Transações \n[2]Filtrar Por Categoria \n[3]Porquinho \n")
-    primeira_escolha_de_funcao = input()
+        #Caso o usuário opte por digitar 1 (Transações), o codigo seguirá esse rumo, onde segue a mesma lógica do primeiro menu
+        if (primeira_escolha_do_usuario == 1):
+            opcoes_do_segundo_menu = [" --> Ler todas as transações", " --> Adicionar uma transação", " --> Atualizar transação existente", " --> Deletar uma transação"]
+            segundo_menu(primeira_escolha_do_usuario)
+            print("Digite o número da ação que deseja realizar: \n[1]Ler todas as transações \n[2]Adicionar uma transação \n[3]Atualizar transação existente \n[4]Deletar uma transação \n")
+            segunda_escolha_do_usuario = int(input())
+            terceiro_menu(primeira_escolha_do_usuario, segunda_escolha_do_usuario)
+            return ['Transações', str(segunda_escolha_do_usuario)]
 
-    if (primeira_escolha_de_funcao == "1"):
-        opcoes_do_segundo_menu = [" --> Ler todas as transações", " --> Adicionar uma transação", " --> Atualizar transação existente", " --> Deletar uma transação"]
-        segundo_menu(primeira_escolha_de_funcao)
-        print("Digite o número da ação que deseja realizar: \n[1]Ler todas as transações \n[2]Adicionar uma transação \n[3]Atualizar transação existente \n[4]Deletar uma transação \n")
-        segunda_escolha_de_funcao = input()
-        terceiro_menu(primeira_escolha_de_funcao, segunda_escolha_de_funcao)
-        return ['Transações', segunda_escolha_de_funcao]
-
-    if (primeira_escolha_de_funcao == "2"):
-        segundo_menu(primeira_escolha_de_funcao)
-        return ['Filtrar', '1']
-
-    if(primeira_escolha_de_funcao == "3"):
-        opcoes_do_segundo_menu = [" --> Criar porquinho", " --> Visualizar porquinho", " --> Deletar porquinho"]
-        segundo_menu(primeira_escolha_de_funcao)
-        print("Digite o número da ação que deseja realizar: \n[1]Criar porquinho \n[2]Visualizar porquinho \n[3]Deletar porquinho \n")
-        segunda_escolha_de_funcao = input()
-        if(segunda_escolha_de_funcao == "2"):
-            opcoes_do_terceiro_menu = [" --> Adicionar dinheiro", " --> Remover dinheiro", " --> Visualizar valor total"]
-            terceiro_menu(primeira_escolha_de_funcao, segunda_escolha_de_funcao)
-            print("Digite o número da ação que deseja realizar: \n[1]Adicionar dinheiro \n[2]Remover dinheiro \n[3]Visualizar valor total \n")
-            terceira_escolha_de_funcao = input()
-            quarto_menu(primeira_escolha_de_funcao, segunda_escolha_de_funcao, terceira_escolha_de_funcao)
-        else:
-            terceiro_menu(primeira_escolha_de_funcao, segunda_escolha_de_funcao)
-            return ['Porquinho',segunda_escolha_de_funcao]
+        #Caso o usuário opte por digitar 2 (Filtrar por categoria), o codigo seguirá esse rumo, onde segue a mesma lógica do primeiro menu
+        if (primeira_escolha_do_usuario == 2):
+            segundo_menu(primeira_escolha_do_usuario)
+            return ['Filtrar', '1']
+            
+        #Caso o usuário opte por digitar 3 (Porquinho), o codigo seguirá esse rumo, onde segue a mesma lógica do primeiro menu
+        if(primeira_escolha_do_usuario == 3):
+            opcoes_do_segundo_menu = [" --> Criar porquinho", " --> Atualizar porquinho", " --> Visualizar porquinhos", " --> Deletar porquinho"]
+            segundo_menu(primeira_escolha_do_usuario)
+            print("Digite o número da ação que deseja realizar: \n[1]Criar porquinho \n[2]Atualizar porquinho \n[3]Visualizar porquinhos \n[4]Deletar porquinho \n")
+            segunda_escolha_do_usuario = int(input())
+            terceiro_menu(primeira_escolha_do_usuario, segunda_escolha_do_usuario)
+            return ['Porquinho',str(segunda_escolha_do_usuario)]
+    except:
+        print("Você digitou um valor inválido, tente novamente apresentando um valor válido.")
